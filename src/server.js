d7ef4ac6649dcb4ef324a236f5e633a484ba8a17
bohdan <bogdanovcharenko1997@gmail.com>
2026-06-30 17:45:07 +0300

    add express server with middleware

--- src/server.js
+++ src/server.js
@@ -7,8 +7,10 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 // Middleware
-app.use(express.json());
 app.use(cors());
+
+app.use(express.json());
+
 app.use(
   pino({
     level: 'info',
@@ -25,14 +27,22 @@ app.use(
   }),
 );
 
-// Перший маршрут
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ "message": "Retrieved all notes" });
 });
 
-// Запуск сервера
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get('/notes/:noteId', (req, res) => {
+  res.status(200).json({ "message": `Retrieved note with ID: ${req.params.noteId}` });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+// Middleware 404 (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ "message": "Route not found" });
 });
 
 app.use((err, req, res, next) => {
@@ -41,8 +51,14 @@ app.use((err, req, res, next) => {
   const isProd = process.env.NODE_ENV === "production";
 
   res.status(500).json({
-    message: isProd
+    "message": isProd
       ? "Something went wrong. Please try again later."
-      : err.message,
+      : err.message
   });
 });
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
