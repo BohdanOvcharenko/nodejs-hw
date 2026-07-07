bd4b57423055088302d8dc067554c6febaab657f
bohdan <bogdanovcharenko1997@gmail.com>
2026-07-07 15:26:31 +0300

    add hw

---
+++ src/controllers/notesController.js
@@ -0,0 +1,20 @@
+import { Note } from '../models/student.js';
+import createHttpError from 'http-errors';
+
+// Отримати список усіх нотаток
+export const getNotes = async (req, res) => {
+  const notes = await Note.find();
+  res.status(200).json(notes);
+};
+
+// Отримати одну нотатку за id
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+	throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
