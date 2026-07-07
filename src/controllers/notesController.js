4ca476a8f5f75d7cbd236c6940ca905e11c189d7
bohdan <bogdanovcharenko1997@gmail.com>
2026-07-07 15:47:03 +0300

    add hw part 1

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -18,3 +18,38 @@ export const getNoteById = async (req, res) => {
 
   res.status(200).json(note);
 };
+
+export const createNote = async (req, res) => {
+  const { title, content } = req.body;
+  const newNote = new Note({ title, content });
+  await newNote.save();
+  res.status(201).json(newNote);
+};
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const deletedNote = await Note.findByIdAndDelete(noteId);
+
+  if (!deletedNote) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(deletedNote);
+};
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+  const { title, content } = req.body;
+
+  const updatedNote = await Note.findByIdAndUpdate(
+    noteId,
+    { title, content },
+    { new: true }
+  );
+
+  if (!updatedNote) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(updatedNote);
+};
--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
-import studentsRoutes from './routes/studentsRoutes.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use(studentsRoutes);
+app.use(notesRoutes);
 
 // Middleware 404 (після всіх маршрутів)
 app.use(notFoundHandler);
